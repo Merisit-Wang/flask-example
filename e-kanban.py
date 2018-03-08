7c7de74aad8ccd3ef11a4befa6b389881ef5f8dc
merisit-wang <merisit-wang@hotmail.com>
2018-03-08 16:44:04 +0800

    add static file and use wtf create form

--- e-kanban.py
+++ e-kanban.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for, redirect
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 manager = Manager(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+class NameForm(FlaskForm):
+    name = StringField('Name', validators=[ DataRequired() ])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
