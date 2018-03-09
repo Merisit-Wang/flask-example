aac2d5f9113c53551bc18f500f6bbacf2faff53c
merisit-wang <merisit-wang@hotmail.com>
2018-03-09 14:46:41 +0800

    add login and logout

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email, Length
 
 class NameForm(FlaskForm):
-    name = StringField('Name', validators=[ DataRequired() ])
+    name = StringField('Name', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Login')
--- app/main/views.py
+++ app/main/views.py
@@ -1,24 +1,6 @@
-from flask import session, render_template, redirect, url_for
+from flask import render_template
 from . import main
-from .forms import NameForm
-from .. import db
-from ..models import User
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form,
-                           name=session.get('name'),
-                           known=session.get('known', False))
+    return render_template('index.html')
